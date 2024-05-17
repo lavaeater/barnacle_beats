e69937600ea6941b756113bb3b1379d275f5f894
Tommie Nygren <tommie.nygren@gmail.com>
2024-05-17 10:06:33 +0200

    More tweaking

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ use bevy::winit::WinitWindows;
 use bevy::DefaultPlugins;
 use barnacle_beats::GamePlugin; // ToDo: Replace bevy_game with your new crate name.
 use std::io::Cursor;
-use winit::window::Icon;
 
 fn main() {
     App::new()
@@ -17,7 +16,7 @@ fn main() {
         .insert_resource(ClearColor(Color::rgb(0.4, 0.4, 0.4)))
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
-                title: "Bevy game".to_string(), // ToDo
+                title: "Barnacle Beats".to_string(), // ToDo
                 // Bind to canvas included in `index.html`
                 canvas: Some("#bevy".to_owned()),
                 // Tells wasm not to override default event handling, like F5 and Ctrl+R
@@ -27,7 +26,7 @@ fn main() {
             ..default()
         }))
         .add_plugins(GamePlugin)
-        .add_systems(Startup, set_window_icon)
+        // .add_systems(Startup, set_window_icon)
         .run();
 }
 
@@ -40,14 +39,14 @@ fn set_window_icon(
     let Some(primary) = windows.get_window(primary_entity) else {
         return;
     };
-    let icon_buf = Cursor::new(include_bytes!(
-        "../build/macos/AppIcon.iconset/icon_256x256.png"
-    ));
-    if let Ok(image) = image::load(icon_buf, image::ImageFormat::Png) {
-        let image = image.into_rgba8();
-        let (width, height) = image.dimensions();
-        let rgba = image.into_raw();
-        let icon = Icon::from_rgba(rgba, width, height).unwrap();
-        primary.set_window_icon(Some(icon));
-    };
+    // let icon_buf = Cursor::new(include_bytes!(
+    //     "../build/macos/AppIcon.iconset/icon_256x256.png"
+    // ));
+    // if let Ok(image) = image::load(icon_buf, image::ImageFormat::Png) {
+    //     let image = image.into_rgba8();
+    //     let (width, height) = image.dimensions();
+    //     let rgba = image.into_raw();
+    //     let icon = Icon::from_rgba(rgba, width, height).unwrap();
+    //     primary.set_window_icon(Some(icon));
+    // };
 }
