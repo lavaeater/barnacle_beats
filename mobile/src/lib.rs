628b3d7142234c5b330a298673a79a872f80a42c
Tommie Nygren <tommie.nygren@gmail.com>
2024-05-17 10:02:05 +0200

    Tweaked it so it builds for me.

--- mobile/src/lib.rs
+++ mobile/src/lib.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 use bevy::window::WindowMode;
-use bevy_game::GamePlugin; // ToDo: Replace bevy_game with your new crate name.
+use barnacle_beats::GamePlugin; // ToDo: Replace bevy_game with your new crate name.
 
 #[bevy_main]
 fn main() {
